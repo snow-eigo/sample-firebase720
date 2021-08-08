6aaf551370e4939c9cdde52c481f1145994d0c6b
Carlo <shiranoyukihime@gmail.com>
2021-08-08 18:08:38 +0800

    added sample messaging

--- Assets/Scripts/MessagingSubscriber.cs
+++ Assets/Scripts/MessagingSubscriber.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MessagingSubscriber : MonoBehaviour
 {
 
+   public Text tokenText;
+   public Text messageText;
+     
+     
+
    private void Start() 
    {
-        Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
-        Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+      Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
+      Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
    }
-   
-   private void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token) {
-        Debug.Log("Received Registration Token: " + token.Token);
+
+   private void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
+   {
+      tokenText.text = token.Token;
+      Debug.Log("Received Registration Token: " + token.Token);
    }
-   
-   private void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e) {
-        Debug.Log("Received a new message from: " + e.Message.From);
+
+   private void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
+   {
+      messageText.text = e.Message.From;
+      Debug.Log("Received a new message from: " + e.Message.From);
    }
 }
